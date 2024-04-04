273f9b8208252e9eac3b7eb249d1f45fc5c1bb0e
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-04-05 04:57:29 +0530

    todo functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import Navbar from "./components/navbar";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, settodo] = useState("");
+  const [todos, settodos] = useState([]);
+  const [finished, setfinished] = useState(false);
+
+  useEffect(() => {
+    let todostring = localStorage.getItem("todos");
+    if (todostring) {
+      let todos = JSON.parse(localStorage.getItem("todos"));
+      if (finished) {
+        const newTodo = todos.filter((todo) => {
+          return todo.isCompleted === true;
+        });
+        settodos(newTodo);
+      } else {
+        settodos(todos);
+      }
+    }
+  }, [finished]);
+
+  const savetoLS = (params) => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  // const handleEdit=() => {
+
+  // }
+  const handleAdd = () => {
+    settodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
+    settodo("");
+    savetoLS();
+  };
+  const handleChange = (e) => {
+    settodo(e.target.value);
+  };
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex((item) => {
+      return item.id === id;
+    });
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    settodos(newTodos);
+    savetoLS();
+  };
+  const handleDelete = (e) => {
+    let id = e.target.name;
+    let newTodos = [];
+    todos.map((todo) => {
+      if (todo.id !== id) {
+        newTodos.push(todo);
+      }
+    });
+    settodos(newTodos);
+    savetoLS();
+  };
+  const handleEdit = (e, id) => {
+    let index = todos.findIndex((item) => {
+      return item.id === id;
+    });
+    settodo(todos[index].todo);
+    let newTodos = [];
+    todos.map((todo) => {
+      if (todo.id !== id) {
+        newTodos.push(todo);
+      }
+    });
+    settodos(newTodos);
+    savetoLS();
+  };
+  const toggleFinished = () => {
+    setfinished(!finished);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-200 min-h-[80vh]">
+        <div className="addTodo my-5">
+          <h2 className="text-lg font-bold">Add a todo </h2>
+          <input
+            value={todo}
+            onChange={handleChange}
+            type="text"
+            className="w-1/2"
+          />
+          <button
+            onClick={handleAdd}
+            disabled={todo.length <= 3}
+            className="bg-violet-700 hover:bg-violet-800 p-2 py-3 text-sm font-bold text-white rounded-md mx-6 disabled:bg-violet-500"
+          >
+            Add
+          </button>
+        </div>
+        <div className="flex p-3">
+          <input
+            className="mx-3"
+            type="checkbox"
+            checked={finished}
+            onChange={toggleFinished}
+          />
+          <h2>Show only finished todos</h2>
+        </div>
+
+        <h1 className="text-xl font-bold">Your Todos</h1>
+        <div className="todos">
+          {todos.map((item) => {
+            return (
+              <div key={item.id} className="todo flex w-1/3 justify-between">
+                <input
+                  name={item.id}
+                  checked={item.isCompleted}
+                  onChange={handleCheckbox}
+                  type="checkbox"
+                  className=""
+                />
+                <div
+                  className={item.isCompleted ? "line-through my-3" : "my-3"}
+                >
+                  {item.todo}
+                </div>
+                <div className="buttons">
+                  <button
+                    onClick={(e) => handleEdit(e, item.id)}
+                    className="bg-violet-700 hover:bg-violet-800 p-2 py-3 text-sm font-bold text-white rounded-md mx-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    name={item.id}
+                    onClick={handleDelete}
+                    className="bg-violet-700 hover:bg-violet-800 p-2 py-3 text-sm font-bold text-white rounded-md mx-1"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
